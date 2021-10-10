b874c5bbe1002d4b9fd4725cb38c232488eeb47a
Deepayan <deepayanmukherjee1312@gmail.com>
2021-10-11 02:21:11 +0530

    Updated routes and schemas

--- server/index.js
+++ server/index.js
@@ -2,8 +2,15 @@ const express = require("express")
 const app = express()
 const dbConnect = require('./db/db')
 
+const userRoute = require("./routes/users")
+const pinRoute = require("./routes/pins")
+
+
 app.use(express.json())
 dbConnect()
+
+app.use("/api/users", userRoute)
+app.use("/api/pins", pinRoute)
 
 app.listen(5000,()=>{
     console.log("Server is running...")
--- server/routes/pins.js
+++ server/routes/pins.js
@@ -3,23 +3,33 @@ const Pin = require("../models/pin");
 
 //create a pin
 router.post("/", async (req, res) => {
-  const newPin = new Pin(req.body);
-  try {
-    const savedPin = await newPin.save();
-    res.status(200).json(savedPin);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+    const newPin = new Pin(req.body);
+    try {
+        const savedPin = await newPin.save();
+        res.status(200).json(savedPin);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 //get all pins
 router.get("/", async (req, res) => {
-  try {
-    const pins = await Pin.find();
-    res.status(200).json(pins);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+
+    const lat = req.query.lat
+    const long = req.query.long
+    
+    try {
+        let pins;
+        if(lat && long){
+            pins = await Pin.find({lat, long})
+        }
+        else{
+            pins = await Pin.find();
+        }
+        res.status(200).json(pins);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;

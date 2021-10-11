801239c6481b43e67cd1f8234446ba7cc5790ec1
Deepayan <deepayanmukherjee1312@gmail.com>
2021-10-12 00:20:14 +0530

    Added markers and popups and fetched data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import { useState, useEffect } from 'react';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import { Room, Star, StarBorder } from "@material-ui/icons";
+import axios from "axios";
 import './App.css';
 
 function App() {
@@ -12,13 +14,81 @@ function App() {
     zoom: 4
   });
 
+  const [pins, setPins] = useState([])
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data)
+        console.log(res)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, [])
+
+
+  const handleMarkerClick = (id, lat, long) => {
+    setCurrentPlaceId(id);
+    setViewport({ ...viewport, latitude: lat, longitude: long });
+  };
+
   return (
     <div className="App">
       <ReactMapGL
       {...viewport}
       mapboxApiAccessToken = {process.env.REACT_APP_MAPBOX}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+      onViewportChange={nextViewport => setViewport(nextViewport)}>
+        {pins.map((p) => (
+          <>
+            <Marker
+              latitude={p.lat}
+              longitude={p.long}
+              offsetLeft={-3.5 * viewport.zoom}
+              offsetTop={-7 * viewport.zoom}
+            >
+              <Room
+                style={{
+                  fontSize: 7 * viewport.zoom,
+                  color: "tomato",
+                  cursor: "pointer",
+                }}
+                onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
+              />
+            </Marker>
+            {p._id === currentPlaceId && (
+              <Popup
+                key={p._id}
+                latitude={p.lat}
+                longitude={p.long}
+                closeButton={true}
+                closeOnClick={false}
+                onClose={() => setCurrentPlaceId(null)}
+                anchor="left"
+              >
+                <div className="card">
+                  <label>Place</label>
+                  <h4 className="place">{p.title}</h4>
+                  <label>Review</label>
+                  <p className="desc">{p.desc}</p>
+                  <label>Rating</label>
+                  <div className="stars">
+                    {Array(p.rating).fill(<Star className="star" />)}
+                  </div>
+                  <label>Information</label>
+                  <span className="username">
+                    Created by <b>{p.username}</b>
+                  </span>
+                  <span className="date">{p.createdAt}</span>
+                </div>
+              </Popup>
+            )}
+          </>
+        ))}
+      </ReactMapGL>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
 const dbConnect = require('./db/db')
 
 const userRoute = require("./routes/users")
 const pinRoute = require("./routes/pins")
 
-
+app.use(cors())
 app.use(express.json())
 dbConnect()
+
+app.get('/', (req,res)=>{
+    res.send('Server is running on 5000...')
+})
 
 app.use("/api/users", userRoute)
 app.use("/api/pins", pinRoute)
 
-app.listen(5000,()=>{
+app.listen(8800,()=>{
     console.log("Server is running...")
 })

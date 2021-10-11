f3d5fc01b02132beba835503d2113cadcba26b80
Deepayan <deepayanmukherjee1312@gmail.com>
2021-10-11 18:32:29 +0530

    Added mapbox setup to project frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 import './App.css';
 
 function App() {
+
+  const [viewport, setViewport] = useState({
+    width: "70vw",
+    height: "100vh",
+    latitude: 46,
+    longitude: 12,
+    zoom: 4
+  });
+
   return (
     <div className="App">
-      React app running...
+      <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken = {process.env.REACT_APP_MAPBOX}
+      onViewportChange={nextViewport => setViewport(nextViewport)}
+    />
     </div>
   );
 }
